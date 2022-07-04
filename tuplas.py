ccf9a9e320afb2724cf86c21de8fd63ff46e47ee
Manuel Tarazona <manutar123@gmail.com>
2022-07-03 20:42:22 -0500

    añadiento tuplas

---
+++ tuplas.py
@@ -0,0 +1,5 @@
+#TUPLAS
+#Son una colección de elementos ordenados que son inmutables
+
+coordenadas = (42.88, 55.16, "Este")#estas se definen en parentésis
+print(len(coordenadas)) # se pueden usar las funciones igual que con las listas
